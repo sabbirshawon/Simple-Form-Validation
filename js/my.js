a935ff93861076e01f33f554df60a766865dd6f8
Sabbir Ahmed <sabbirs93@gmail.com>
2018-10-22 03:58:38 +0600

    Update my.js

--- js/my.js
+++ js/my.js
@@ -2,7 +2,7 @@ var divs = new Array();
     divs[0] = "errFirst";
     divs[1] = "errLast";
     divs[2] = "errEmail";
-    divs[3] = "errUid";
+    divs[3] = "errUname";
     divs[4] = "errPassword";
     divs[5] = "errConfirm";
     function validate()
@@ -11,14 +11,14 @@ var divs = new Array();
       inputs[0] = document.getElementById('first').value;
       inputs[1] = document.getElementById('last').value;
       inputs[2] = document.getElementById('email').value;
-      inputs[3] = document.getElementById('uid').value;
+      inputs[3] = document.getElementById('uname').value;
       inputs[4] = document.getElementById('password').value;
       inputs[5] = document.getElementById('confirm').value;
       var errors = new Array();
       errors[0] = "<span style='color:red'>Please enter your first name!</span>";
       errors[1] = "<span style='color:red'>Please enter your last name!</span>";
       errors[2] = "<span style='color:red'>Please enter your email!</span>";
-      errors[3] = "<span style='color:red'>Please enter your user id!</span>";
+      errors[3] = "<span style='color:red'>Please enter your user name!</span>";
       errors[4] = "<span style='color:red'>Please enter your password!</span>";
       errors[5] = "<span style='color:red'>Please confirm your password!</span>";
       for (i in inputs)
